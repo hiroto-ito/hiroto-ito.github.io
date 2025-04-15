9936a73afb439f6bee8846eab36177dc61188fed
hiroto ito <worldsenddance@gmail.com>
2025-04-15 17:00:21 +0900

    2025/04/15

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ window.addEventListener("scroll", () => {
     header.classList.toggle("Header-scrolled", window.scrollY > 200);
     //window.scrollY
 });
+//sections スクロールフェードイン
+window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const sections = document.querySelectorAll('.sections-rest');
+    
+    sections.forEach( (section) => {
+        const distanceToBox = section.offsetTop;
+        // 下記条件が成り立つときだけboxにis-activeクラスを付与する
+        console.log(section);
+        console.log(scroll);
+        console.log(windowHeight);
+        console.log(distanceToBox);
+        if(scroll + windowHeight > distanceToBox) {
+            section.classList.add('sections-scrolled');
+        }
+    });
+});
